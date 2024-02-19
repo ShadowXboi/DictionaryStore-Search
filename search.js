d79c0457491ef454fc0033810d1c090e90a1d8d6
Tushar <97547800+ShadowXboi@users.noreply.github.com>
2024-02-18 22:21:08 -0800

    Update search.js

--- search.js
+++ search.js
@@ -14,7 +14,7 @@ function searchWord(event) {
   // Initializes a new XMLHttpRequest object for asynchronous HTTP request.
   const xhr = new XMLHttpRequest();
   // Configures the request: Method (GET), URL, and asynchronous flag (true by default).
-  xhr.open("GET", `https://jacksoncomp4537lab4-v5i5b.ondigitalocean.app/api/definitions/?word=${word}`);
+  xhr.open("GET", `https://jacksoncomp4537lab4-v5i5b.ondigitalocean.app/api/definitions?word=${word}`);
 
   // Defines what happens when the request receives a response.
   xhr.onload = () => {
